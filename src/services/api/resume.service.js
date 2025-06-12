014b6ec1c78765f4ea6c9d1589b5656bdf1bb07e
Reza Bagheri <bagherbagherreza@gmail.com>
2025-06-12 07:47:54 +0330

    feat: implement getResume API function and remove unused ContactInfo component

--- src/services/api/resume.service.js
+++ src/services/api/resume.service.js
@@ -2,6 +2,14 @@ import axios from '../axios';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
+export const getResume = () => {
+  return axios({
+    method: 'GET',
+    baseURL,
+    url: '/resume',
+  });
+};
+
 export const downloadResume = (templateId) => {
   return axios({
     method: 'GET',
